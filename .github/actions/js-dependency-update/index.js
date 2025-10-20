763c963b5525fea4e0d5e27de000d73dd57484e6
junechol <41774939+junechol-ahn@users.noreply.github.com>
2025-10-20 02:25:09 +0000

    js to read inputs

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,7 +1,14 @@
-import { info } from '@actions/core';
+import { getBooleanInput, getInput, info } from '@actions/core';
 
 async function run() {
-    info('I am a custrom JS action!');
+  const baseBranch = getInput('base-branch');
+  const targetBranch = getInput('target-branch');
+  const workingDirectory = getInput('working-directory');
+  const githubToken = getInput('gh-token');
+  const isDebug = getBooleanInput('debug');
+
+
+  info(baseBranch, targetBranch, workingDirectory, githubToken, isDebug);
 }
 
 run();
