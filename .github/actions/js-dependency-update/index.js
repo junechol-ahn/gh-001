d6e3ed48569fa221887c1875bf1e26d2ba8d4244
junechol <41774939+junechol-ahn@users.noreply.github.com>
2025-10-20 04:41:07 +0000

    action runs npm update

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,14 +1,53 @@
-import { getBooleanInput, getInput, info } from '@actions/core';
+import core from '@actions/core';
+import exec from '@actions/exec';
+
+const validateBranchName = ({ branchName }) =>
+  /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
+const validateDirectoryName = ({ dirName }) =>
+  /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
 async function run() {
-  const baseBranch = getInput('base-branch');
-  const targetBranch = getInput('target-branch');
-  const workingDirectory = getInput('working-directory');
-  const githubToken = getInput('gh-token');
-  const isDebug = getBooleanInput('debug');
+  const baseBranch = core.getInput('base-branch');
+  const targetBranch = core.getInput('target-branch');
+  const workingDirectory = core.getInput('working-directory');
+  const githubToken = core.getInput('gh-token');
+  const debug = core.getBooleanInput('debug');
+
+  core.setSecret(githubToken);
+
+  if (!validateBranchName({ branchName: baseBranch })) {
+    core.setFailed(`Invalid base-branch name: ${baseBranch}`);
+    return;
+  }
+
+  if (!validateBranchName({ branchName: targetBranch })) {
+    core.setFailed(`Invalid target-branch name: ${targetBranch}`);
+    return;
+  }
+
+  if (!validateDirectoryName({ dirName: workingDirectory })) {
+    core.setFailed(`Invalid working-directory name: ${workingDirectory}`);
+    return;
+  }
+
+  core.info(`[js-dependency-update] : base-branch: ${baseBranch}`);
+  core.info(`[js-dependency-update] : target-branch: ${targetBranch}`);
+  core.info(
+    `[js-dependency-update] : working-directory: ${workingDirectory}`
+  );
+  core.info(`[js-dependency-update] : debug: ${debug}`);
+
+  await exec.exec('npm update', [], { cwd: workingDirectory });
+
+  const gitStatus = await exec.getExecOutput('git status -s package*.json', [], { cwd: workingDirectory });
+
+  if (gitStatus.stdout === '') {
+    core.info('[js-dependency-update] : No dependency updates found.');
+    return;
+  }
 
 
-  info(`${baseBranch}, ${targetBranch}, ${workingDirectory}, ${githubToken}, ${isDebug}`);
+  core.info(`git status:\n${gitStatus.stdout}`);
 }
 
 run();
