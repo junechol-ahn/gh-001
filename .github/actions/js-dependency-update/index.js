e1834626bb56dfd5e851eb3829fd4ef39f42303e
junechol <41774939+junechol-ahn@users.noreply.github.com>
2025-10-20 04:53:26 +0000

    dot is permitted in directory name

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -4,7 +4,7 @@ import exec from '@actions/exec';
 const validateBranchName = ({ branchName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
 const validateDirectoryName = ({ dirName }) =>
-  /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
+  /^[a-zA-Z0-9_\-\.\/]+$/.test(dirName);
 
 async function run() {
   const baseBranch = core.getInput('base-branch');
