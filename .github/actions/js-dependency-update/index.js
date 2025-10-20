89d466e3ad445c1287b75e158edd2600857c9472
junechol <41774939+junechol-ahn@users.noreply.github.com>
2025-10-20 06:40:50 +0000

    more action'

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -1,5 +1,6 @@
 import core from '@actions/core';
 import exec from '@actions/exec';
+import github from '@actions/github';
 
 const validateBranchName = ({ branchName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName);
@@ -7,11 +8,15 @@ const validateDirectoryName = ({ dirName }) =>
   /^[a-zA-Z0-9_\-\.\/]+$/.test(dirName);
 
 async function run() {
-  const baseBranch = core.getInput('base-branch');
-  const targetBranch = core.getInput('target-branch');
-  const workingDirectory = core.getInput('working-directory');
-  const githubToken = core.getInput('gh-token');
+  const baseBranch = core.getInput('base-branch', { required: true });
+  const targetBranch = core.getInput('target-branch', { required: true });
+  const workingDirectory = core.getInput('working-directory', { required: true });
+  const githubToken = core.getInput('gh-token', { required: true });
   const debug = core.getBooleanInput('debug');
+
+  const commonExecOpts = {
+    cwd: workingDirectory,
+  }
 
   core.setSecret(githubToken);
 
@@ -37,13 +42,37 @@ async function run() {
   );
   core.info(`[js-dependency-update] : debug: ${debug}`);
 
-  await exec.exec('npm update', [], { cwd: workingDirectory });
+  await exec.exec('npm update', [], { ...commonExecOpts });
 
-  const gitStatus = await exec.getExecOutput('git status -s package*.json', [], { cwd: workingDirectory });
+  const gitStatus = await exec.getExecOutput('git status -s package*.json', [], { ...commonExecOpts });
 
   if (gitStatus.stdout === '') {
     core.info('[js-dependency-update] : No dependency updates found.');
     return;
+  }
+
+  await exec.exec(`git config --global user.name "github-actions[bot]"`);
+  await exec.exec(`git config --global user.email "github-actions[bot]@users.noreply.github.com"`);
+  await exec.exec(`git checkout -b ${targetBranch}`, [], { ...commonExecOpts });
+  await exec.exec(`git add package*.json`, [], { ...commonExecOpts });
+  await exec.exec(`git commit -m "chore: update dependencies"`, [], { ...commonExecOpts });
+  await exec.exec(
+    `git push -u origin ${targetBranch} --force`, [], { ...commonExecOpts }
+  );
+
+  const oktokit = github.getOctokit(githubToken);
+  try {
+    await oktokit.rest.pulls.create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title: `Dependency Update`,
+      body: `Automated dependency update by js-dependency-update action.`,
+      head: targetBranch,
+      base: baseBranch,
+    })
+  } catch (e) {
+    core.warning('[js-dependency-update] : Failed to create pull request. Check logs for details.');
+    core.warning(e.message);
   }
 
 
