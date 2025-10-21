c6bad5ba1f79be5404e3cc92335d3cd6e2e153ff
junechol <41774939+junechol-ahn@users.noreply.github.com>
2025-10-21 04:32:58 +0000

    docker custom action to send back output

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -32,7 +32,11 @@ def run():
   delay = int(os.getenv("INPUT_DELAY", "5"))
   max_trials = int(os.getenv("INPUT_MAX_TRIALS", "10"))
 
+
   website_reachable = ping_url(url, delay, max_trials)
+
+  output_file = os.getenv("GITHUB_OUTPUT")
+  open(output_file, "a").write(f"url-reachable={website_reachable}\n")
 
   if not website_reachable:
     raise Exception(f"Could not reach {url} after {max_trials} attempts.")
