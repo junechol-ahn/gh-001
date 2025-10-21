dd48c28ae3289ed4d94b75b40b09331d6f6e87fb
junechol <41774939+junechol-ahn@users.noreply.github.com>
2025-10-21 02:21:24 +0000

    python action pings on request

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,43 @@
+import os
+import time
+import requests
+
+
+
+
+def ping_url(url, delay, max_trials): 
+  trials = 0
+
+  while trials < max_trials:
+      try:
+          response = requests.get(url)
+          if response.status_code == 200:
+              print(f"Successfully reached {url}")
+              return True
+          else:
+              print(f"Failed to reach {url}, status code: {response.status_code}")
+      except requests.RequestException as e:
+          print(f"Error reaching {url}: {e}")
+      
+      trials += 1
+      if trials < max_trials:
+          print(f"Retrying in {delay} seconds... ({max_trials - trials} attempts left)")
+          time.sleep(delay)
+
+  print(f"Failed to reach {url} after {max_trials} attempts.")
+  return False
+
+def run():
+  url = os.getenv("INPUT_URL")
+  delay = int(os.getenv("INPUT_DELAY", "5"))
+  max_trials = int(os.getenv("INPUT_MAX_TRIALS", "10"))
+
+  website_reachable = ping_url(url, delay, max_trials)
+
+  if not website_reachable:
+    raise Exception(f"Could not reach {url} after {max_trials} attempts.")
+
+  print(f"Website {url} is reachable.")
+
 if __name__ == "__main__":
-  print("Hello world")
+  run()
